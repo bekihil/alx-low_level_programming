92fd8e5606514289580333ded3dd319f45f2f2f8
bekihil <40001027+bekihil@users.noreply.github.com>
2022-04-08 16:56:43 +0800

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "holberton.h"
-
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+* main - multiply 2 numbers as arguments
+* @argc: number of arguments
+* @argv: arguments
+* Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		printf("Hel, World, World, Hel.\n");
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
-		return (0);
 	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
+}
